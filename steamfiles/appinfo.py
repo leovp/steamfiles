7b204765cd496c5b1c700f42a025adb137408278
Leonid R <leovp@users.noreply.github.com>
2016-04-22 10:29:45 +0300

    Add boilerplate Appinfo code

---
+++ steamfiles/appinfo.py
@@ -0,0 +1,18 @@
+VDF_VERSION = 0x07564426
+VDF_UNIVERSE = 0x00000001
+
+
+def load():
+    raise NotImplementedError
+
+
+def loads():
+    raise NotImplementedError
+
+
+def dump():
+    raise NotImplementedError
+
+
+def dumps():
+    raise NotImplementedError

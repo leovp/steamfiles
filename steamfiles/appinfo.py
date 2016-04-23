0a5485bd2cb6093f0fc8575fc616cde87b2ef9bc
Leonid R <leovp@users.noreply.github.com>
2016-04-23 16:38:32 +0300

    Prepare appinfo load/dump functions

--- steamfiles/appinfo.py
+++ steamfiles/appinfo.py
@@ -2,17 +2,17 @@ VDF_VERSION = 0x07564426
 VDF_UNIVERSE = 0x00000001
 
 
-def load():
+def load(fp):
+    return loads(fp.read())
+
+
+def loads(content):
     raise NotImplementedError
 
 
-def loads():
-    raise NotImplementedError
+def dump(fp, obj):
+    fp.write(dumps(obj))
 
 
-def dump():
-    raise NotImplementedError
-
-
-def dumps():
+def dumps(obj):
     raise NotImplementedError

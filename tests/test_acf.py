5256f2f40d31888b6afe82dcc261b22b30760e56
Leonid R <leovp@users.noreply.github.com>
2017-02-19 16:41:49 +0300

    Implement custom containers for ACF.
    load() & loads() now parse data into a `dict` instead of `OrderedDict`.
    Additionally, they now take an optional argument `wrapper`.
    Parsed key-value pairs are processed with `wrapper`, so you can
    choose any container for the data, provided it's somewhat compatible
    with `dict`. `dict` is also the default container when you don't specify
    another one.

--- tests/test_acf.py
+++ tests/test_acf.py
@@ -1,6 +1,7 @@
 import io
 import os
 import pytest
+from collections import OrderedDict
 from steamfiles import acf
 
 test_file_name = os.path.join(os.path.dirname(__file__), 'test_data/appmanifest_202970.acf')
@@ -18,9 +19,17 @@ def test_acf_keys_exist(acf_data):
     assert 'BytesDownloaded' in data['AppState']['DlcDownloads']['202988']
     assert 'BytesToDownload' in data['AppState']['DlcDownloads']['202988']
 
+
 @pytest.mark.usefixtures('acf_data')
 def test_loads_dumps(acf_data):
     assert acf.dumps(acf.loads(acf_data)) == acf_data
+
+
+@pytest.mark.usefixtures('acf_data')
+def test_loads_dumps_with_wrapper(acf_data):
+    loaded = acf.loads(acf_data, wrapper=OrderedDict)
+    assert isinstance(loaded, OrderedDict)
+    assert acf.dumps(loaded) == acf_data
 
 
 @pytest.mark.usefixtures('acf_data')
@@ -32,6 +41,20 @@ def test_load_dump(acf_data):
 
     # Rewind to the beginning
     out_file.seek(0)
+    assert out_file.read() == acf_data
+
+
+@pytest.mark.usefixtures('acf_data')
+def test_load_dump_with_wrapper(acf_data):
+    with open(test_file_name, 'rt') as in_file:
+        out_file = io.StringIO()
+        loaded = acf.load(in_file, wrapper=OrderedDict)
+        acf.dump(loaded, out_file)
+
+    # Rewind to the beginning
+    out_file.seek(0)
+
+    assert isinstance(loaded, OrderedDict)
     assert out_file.read() == acf_data
 
 

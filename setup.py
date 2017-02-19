0bf1eb3fd64e5da4024ab6c6ff91cf12d05db9ec
Leonid R <leovp@users.noreply.github.com>
2017-02-19 23:25:44 +0300

    Fix a typo in a classifier :(

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Lauguage :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6',
     ],
 )

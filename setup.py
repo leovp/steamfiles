49e6de0cd42d1387e2521b339339c5b6eda6883a
WinterPhoenix <winterphoenix96@protonmail.com>
2020-06-19 02:40:56 -0400

    Don't use Pip Internals to parse requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
 import os
 import setuptools
-try:
-    # pip >=20
-    from pip._internal.network.session import PipSession
-    from pip._internal.req import parse_requirements
-except ImportError:
-    try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.download import PipSession
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        # pip <= 9.0.3
-        from pip.download import PipSession
-        from pip.req import parse_requirements
-
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
@@ -38,15 +24,9 @@ setuptools.setup(
     platforms=['any'],
     packages=setuptools.find_packages(exclude=['tests']),
 
-    install_requires=[
-        str(req.req) for req in parse_requirements('requirements.txt',
-                                                   session=PipSession())
-        ],
+    install_requires=read('requirements.txt').splitlines(),
 
-    tests_require=[
-        str(req.req) for req in parse_requirements('requirements_test.txt',
-                                                   session=PipSession())
-        ],
+    tests_require=read('requirements_test.txt').splitlines(),
 
     classifiers=[
         'Development Status :: 4 - Beta',

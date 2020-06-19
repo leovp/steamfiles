392e86652b631140dc4a9779762aa7927171f033
WinterPhoenix <winterphoenix96@protonmail.com>
2020-06-19 02:28:51 -0400

    Fix Requirements parsing with Pip 20+

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import os
 import setuptools
-try: # for pip >= 10
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
-    from pip._internal.download import PipSession
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-    from pip.download import PipSession
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 
 
 def read(fname):
